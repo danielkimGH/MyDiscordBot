1a4ec557a4a1ece6338f72efeebf5cf5e907cf2a
Daniel Kim <kimdanieljung@gmail.com>
2024-08-21 18:04:32 -0700

    Implemented add to queue function

--- mybot.py
+++ mybot.py
@@ -6,9 +6,34 @@ from dotenv import load_dotenv
 
 
 class MyClient(discord.Client):
+
+    def _get_track_URI(self, url_link: str):
+        """
+        Returns Spotify track URI (str), None otherwise
+
+        Input: Spotify track URL (str)
+        """
+        url_link_list = url_link.split("/")
+
+        if len(url_link_list) <= 1 or url_link_list[-2] != "track":
+            return None
+
+        uri_index = url_link_list.index('track') + 1
+        uri = url_link_list[uri_index].split('?si')[0]
+
+        # Track URI validity check
+        try:
+            uri_check = sp.audio_features(uri)
+            if uri_check[0] is None:
+                return None
+        except:
+            return None
+
+        return f"spotify:track:{uri}"
+
     # Event: Called when client login is successful
     async def on_ready(self):
-        print(f'Logged in as {self.user}')
+        print(f'Bot activated as {self.user}')
 
     # Event: Called when a Message is created and sent
     async def on_message(self, message):
@@ -16,49 +41,56 @@ class MyClient(discord.Client):
         if message.author.id == self.user.id:
             return
 
-        # Reply to command '!hello', mention_author parameter is for direct reply (e.g. @user-name)
-        if message.content.startswith('!hello'):
-            await message.reply('Hello there!', mention_author=True)
-
-        # Adds reaction to message for a quick yes/no poll
-        if message.content.startswith('YES/NO') or message.content.startswith('yes/no'):
-            yes = '\N{THUMBS UP SIGN}'
-            no = '\N{THUMBS DOWN SIGN}'
-            await message.add_reaction(yes)
-            await message.add_reaction(no)
-
-        # Show song queue list
-        if message.content.startswith('!queue'):
-            queue_list = []
-
-            for item in sp.queue()['queue']:
-                artist_name = item['artists'][0]['name']
-                song_name = item['name']
-                if len(queue_list) <= 5:
-                    queue_list.append(f"* {artist_name} : {song_name}\n")
-                else:
-                    break
-
-            if queue_list:
-                await message.channel.send(f'>>> **Queue** *(next 5)*\n'
-                                           f'{"".join(queue_list)}')
+        # Get current song
+        if message.content == '!song':
+            track_object = sp.current_playback()
+            if track_object is None or track_object['currently_playing_type'] != 'track':
+                await message.channel.send(f'>>> **No songs in play**')
             else:
-                await message.channel.send(">>> Queue is empty")
+                artist_name = track_object['item']['artists'][0]['name']
+                song_name = track_object['item']['name']
+                song_URL = track_object['item']['external_urls']['spotify']
+                await message.channel.send(f'>>> :headphones: **Now Playing**\n'
+                                           f'**Artist:** {artist_name}\n'
+                                           f'**Song:** [{song_name}](<{song_URL}>)')
 
-    ## Events: Shows current song playing on Spotify
-    async def on_presence_update(self, before, after):
-        if after.activities:
-            for activity in after.activities:
-                if isinstance(activity, discord.Spotify):
-                    channel = discord.utils.get(after.guild.text_channels, name="general")
-                    if channel:
-                        await channel.send(f'>>> :headphones: **Now Playing**\n'
-                                           f'**Artist:** {activity.artist}\n'
-                                           f'**Song:** [{activity.title}](<{activity.track_url}>)')
+        # Get song queue list
+        if message.content == '!queue':
+            queue_list = []
+            queue_object = sp.queue()
+            queue_display_limit = 5
+
+            if queue_object is None or queue_object['currently_playing']['type'] == 'episode':
+                await message.channel.send(">>> **Queue is empty**")
+            else:
+                for item in queue_object['queue']:
+                    artist_name = item['artists'][0]['name']
+                    song_name = item['name']
+                    if len(queue_list) < queue_display_limit:
+                        queue_list.append(f"* {artist_name} - {song_name}\n")
+                    else:
+                        break
+                await message.channel.send(f'>>> **Queue** *(next {queue_display_limit})*\n'
+                                           f'{"".join(queue_list)}')
+
+        # Add song to queue
+        if message.content.startswith('!add'):
+            track_uri = self._get_track_URI(message.content)
+            if not track_uri:
+                await message.channel.send(f'>>> **Invalid track URL**')
+            else:
+                sp.add_to_queue(track_uri)
+                await message.add_reaction('\N{BLACK RIGHT-POINTING TRIANGLE}')
+
 
 # Load token
 load_dotenv()
-token = os.getenv("DISCORD_BOT_TOKEN")
+discord_token = os.getenv("DISCORD_BOT_TOKEN")
+scope = "user-read-playback-state,user-modify-playback-state"
+sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+                                                             client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+                                                             redirect_uri=os.getenv("SPOTIFY_REDIRECT_URI"),
+                                                             scope=scope))
 
 # Intents specifies which events the bot will receive from Discord
 # default enables everything except presences, members, and message_content
@@ -67,11 +99,5 @@ intents.message_content = True
 intents.presences = True
 intents.members = True
 
-scope = "user-read-playback-state,user-modify-playback-state"
-sp = spotipy.Spotify(client_credentials_manager=SpotifyOAuth(client_id=os.getenv("SPOTIFY_CLIENT_ID"),
-                                                             client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
-                                                             redirect_uri=os.getenv("SPOTIFY_REDIRECT_URI"),
-                                                             scope=scope))
-
 client = MyClient(intents=intents)
-client.run(token)
+client.run(discord_token)
